db4cd28f4ed9410f62fca451020bba1beea0364d
dfox <dfox@foxartbox.com>
2023-09-02 23:23:21 +0300

    Social links fix

--- wp-content/themes/tv2023/template-parts/social.php
+++ wp-content/themes/tv2023/template-parts/social.php
@@ -1,27 +1,21 @@
 <ul class="social_list">
 	<?php // all options https://tatyanavoilokova.com/wp-admin/admin.php?page=theme-custom-options
-	$instagram      = esc_attr(get_option('theme_inst'));
-	$telegram       = esc_attr(get_option('theme_telegram'));
-	$whatsapp       = esc_attr(get_option('theme_whatsapp'));
+	$instagram      = esc_attr(get_option('theme_inst')) ? esc_attr(get_option('theme_inst')) : 'tatiana.voil';
+	$telegram       = esc_attr(get_option('theme_telegram')) ? esc_attr(get_option('theme_telegram')) : '359876700150';
+	$whatsapp       = esc_attr(get_option('theme_whatsapp')) ? esc_attr(get_option('theme_whatsapp')) : '359876700150';?>
 
-	// whatsapp
-	if (isset($whatsapp)) { ?>
-		<li class="social_list__item">
-			<a class="social_list__icon social_list__icon--whatsapp" href="//wa.me/<?= $whatsapp; ?>" target="_blank"></a>
-		</li>
-	<?php }
+	<?php // whatsapp ?>
+	<li class="social_list__item">
+		<a class="social_list__icon social_list__icon--whatsapp" href="//wa.me/<?= $whatsapp; ?>" target="_blank"></a>
+	</li>
 
-	// telegram
-	if (isset($telegram)) { ?>
-		<li class="social_list__item">
-			<a class="social_list__icon social_list__icon--telegram" href="//t.me/<?= $telegram; ?>" target="_blank"></a>
-		</li>
-	<?php }
+	<?php // telegram ?>
+	<li class="social_list__item">
+		<a class="social_list__icon social_list__icon--telegram" href="//t.me/<?= $telegram; ?>" target="_blank"></a>
+	</li>
 
-	// instagram
-	if (isset($instagram)) { ?>
-		<li class="social_list__item">
-			<a class="social_list__icon social_list__icon--instagram" href="//www.instagram.com/<?= $instagram; ?>" target="_blank"></a>
-		</li>
-	<?php } ?>
+	<?php // instagram ?>
+	<li class="social_list__item">
+		<a class="social_list__icon social_list__icon--instagram" href="//www.instagram.com/<?= $instagram; ?>" target="_blank"></a>
+	</li>
 </ul>
