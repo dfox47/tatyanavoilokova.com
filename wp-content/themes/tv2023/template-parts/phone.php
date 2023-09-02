f1143c2bd2f3cc590dfe7f8ba2391667e101d203
dfox <dfox@foxartbox.com>
2023-09-02 23:24:25 +0300

    Phone php ref

--- wp-content/themes/tv2023/template-parts/phone.php
+++ wp-content/themes/tv2023/template-parts/phone.php
@@ -1,10 +1,5 @@
 <?php // all options https://tatyanavoilokova.com/wp-admin/admin.php?page=theme-custom-options
-$phone = esc_attr(get_option('theme_phone'));
-
-if (empty($phone)) {
-	$phone = '+359 (87) 670-0150';
-}
-
+$phone      = esc_attr(get_option('theme_phone')) ? esc_attr(get_option('theme_phone')) : '+359 (87) 670-0150';
 $phoneShort = preg_replace('/[()\s-]/', '', $phone); ?>
 
 <a class="phone" href="tel:<?= $phoneShort; ?>"><?= $phone; ?></a>
