32ce39554ef7f8e7c548b49868c023b18eca6dac
dfox <dfox@foxartbox.com>
2023-09-02 23:05:09 +0300

    Base files. Part 5

--- wp-content/themes/tv2023/template-parts/phone.php
+++ wp-content/themes/tv2023/template-parts/phone.php
@@ -1,9 +1,9 @@
 
-<?php // all options https://brokertop.ru/wp-admin/admin.php?page=theme-custom-options
+<?php // all options https://tatyanavoilokova.com/wp-admin/admin.php?page=theme-custom-options
 $phone = esc_attr(get_option('broker_phone'));
 
 if ( isset($phone) ) {
 	$phoneShort = preg_replace('/[()\s-]/', '', $phone); ?>
 
-	<a class="phone" href="tel:<?php echo $phoneShort; ?>"><?php echo $phone; ?></a>
+	<a class="phone" href="tel:<?= $phoneShort; ?>"><?= $phone; ?></a>
 <?php } ?>
