127e2180bf5fd8ff74a6e54e66aa03105cc7ee9e
dfox <dfox@foxartbox.com>
2023-09-19 15:07:00 +0300

    Footer. Hide sidebar. Add menu + phone + social

--- wp-content/themes/tv2023/footer.php
+++ wp-content/themes/tv2023/footer.php
@@ -2,39 +2,19 @@
 $i = esc_url(get_template_directory_uri()) . '/i'; ?>
 
 <?php if (is_active_sidebar('footer')) : ?>
-	<?php dynamic_sidebar('footer'); ?>
+<!--	--><?php //dynamic_sidebar('footer'); ?>
 <?php endif; ?>
 
 <footer class="footer">
-	<div class="wrap">
-		<div class="footer_wrap2">
-			<div class="footer_wrap">
-				<?php // footer_menu
-				wp_nav_menu(array(
-					'container'         => false,
-					'depth'             => 2,
-					'item_spacing'      => 'preserve',
-					'items_wrap'        => '<ul class="%2$s">%3$s</ul>',
-					'menu'              => 'footer_menu',
-					'menu_class'        => 'footer_menu',
-				)); ?>
-			</div>
-
-			<div class="footer_bottom">
-				<div class="footer_phone">
-					<?php include "template-parts/phone.php"; ?>
-				</div>
-
-				<?php // copyright
-				include "template-parts/copyright.php"; ?>
-
-				<div class="footer_social">
-					<?php // social
-					include "template-parts/social.php"; ?>
-				</div>
-			</div>
-		</div>
+	<div class="footer_phone">
+		<?php include "template-parts/phone.php"; ?>
 	</div>
+
+	<?php // copyright
+	include "template-parts/copyright.php"; ?>
+
+	<?php // social
+	include "template-parts/social.php"; ?>
 </footer>
 
 <?php // popup
