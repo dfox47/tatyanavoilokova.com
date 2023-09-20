ba1b5c560e2b50c509d7bfc2b093835365ecc930
dfox <dfox@foxartbox.com>
2023-09-20 21:21:19 +0300

    JS. Bubbles

---
+++ wp-content/themes/tv2023/js/bubbles.js
@@ -0,0 +1,19 @@
+// bubbles.js [START]
+const $bubbles = document.querySelector('.js-bubbles')
+
+setInterval(() => {
+	const bubble    = document.createElement('span')
+	const duration  = Math.floor(Math.random() * 5000 + 3000)
+
+	bubble.classList.add('bubble')
+	bubble.style.animationDuration  = duration + 'ms'
+	bubble.style.left               = Math.floor(Math.random() * $bubbles.clientWidth) + 'px'
+	bubble.style.width              = Math.floor(Math.random() * 30 + 5) + 'px'
+
+	$bubbles.append(bubble)
+
+	setTimeout(() => {
+		bubble.remove()
+	}, duration + 1000)
+}, 1000)
+// bubbles.js [END]
