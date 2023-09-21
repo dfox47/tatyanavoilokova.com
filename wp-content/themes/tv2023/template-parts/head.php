44bedf5f29e56fefbdd9830e125c5dc998bdbf53
dfox <dfox@foxartbox.com>
2023-09-21 09:49:16 +0300

    CSS. Bubbles to independent file

--- wp-content/themes/tv2023/template-parts/head.php
+++ wp-content/themes/tv2023/template-parts/head.php
@@ -8,7 +8,7 @@ if (get_the_post_thumbnail_url()) {
 // page type
 $currentUrl = $_SERVER['REQUEST_URI']; ?>
 
-<header class="header">
+<header id="home" class="header js-hash-on-scroll">
 	<div class="header_left">
 		<a class="logo" href="/"><span class="js-img-scroll" data-src="" title=""></span></a>
 
