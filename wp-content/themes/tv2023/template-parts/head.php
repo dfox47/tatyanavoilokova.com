fcee4ea3aaf0d6d902df86881e1d53c3eec94c99
dfox <dfox@foxartbox.com>
2023-09-23 15:59:33 +0300

    Default topmenu

--- wp-content/themes/tv2023/template-parts/head.php
+++ wp-content/themes/tv2023/template-parts/head.php
@@ -15,14 +15,21 @@ $currentUrl = $_SERVER['REQUEST_URI']; ?>
 		<div class="header_menu_toggle js-header-menu-toggle"></div>
 
 		<?php // header_menu
-		wp_nav_menu(array(
-			'container'         => false,
-			'depth'             => 0,
-			'item_spacing'      => 'preserve',
-			'items_wrap'        => '<ul class="%2$s">%3$s</ul>',
-			'menu'              => 'header_menu',
-			'menu_class'        => 'header_menu',
-		)); ?>
+//		wp_nav_menu(array(
+//			'container'         => false,
+//			'depth'             => 0,
+//			'item_spacing'      => 'preserve',
+//			'items_wrap'        => '<ul class="%2$s">%3$s</ul>',
+//			'menu'              => 'header_menu',
+//			'menu_class'        => 'header_menu',
+//		)); ?>
+
+		<ul class="header_menu">
+			<li><a class="js-scroll-to" href="#home">Home</a></li>
+			<li><a class="js-scroll-to" href="#about">About me</a></li>
+			<li><a class="js-scroll-to" href="#portfolio">Portfolio</a></li>
+			<li><a class="js-scroll-to" href="#contacts">Contacts</a></li>
+		</ul>
 	</div>
 
 	<div class="header_right">
