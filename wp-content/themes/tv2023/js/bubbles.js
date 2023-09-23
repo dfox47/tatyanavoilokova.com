aaa5926468c0fa89524a04f894575158c80ca0ea
dfox <dfox@foxartbox.com>
2023-09-23 20:22:40 +0300

    JS. Bubbles. Make it for all hero block

--- wp-content/themes/tv2023/js/bubbles.js
+++ wp-content/themes/tv2023/js/bubbles.js
@@ -1,11 +1,17 @@
 // bubbles.js [START]
-const bubblesGenerator = () => {
-	const $bubbles = document.querySelector('.js-bubbles')
+const bubblesGenerator = (placeTo, interval, bubbleWidth) => {
+	if (placeTo || interval || bubbleWidth) return
+
+	const $bubbles  = document.querySelector(placeTo)
+	const delay     = interval
+	const width     = bubbleWidth
 
 	if (!$bubbles) return
 
+	let bubbleClicks = 0
+
 	// generate bubbles
-	setInterval(() => {
+	const createSmallBubbles = setInterval(() => {
 		const bubble    = document.createElement('span')
 		const duration  = Math.floor(Math.random() * 5000 + 3000)
 
@@ -13,19 +19,28 @@ const bubblesGenerator = () => {
 		bubble.classList.add('js-bubble')
 		bubble.style.animationDuration  = duration + 'ms'
 		bubble.style.left               = Math.floor(Math.random() * $bubbles.clientWidth) + 'px'
-		bubble.style.width              = Math.floor(Math.random() * 40 + 5) + 'px'
+		bubble.style.width              = Math.floor(Math.random() * width + 5) + 'px'
 
 		$bubbles.append(bubble)
 
-		bubble.addEventListener('click', () => {
-			bubble.remove()
+		bubble.addEventListener('click', (e) => {
+			e.target.remove()
+
+			bubbleClicks++
+
+			if (bubbleClicks > 1) {
+				clearInterval(createSmallBubbles)
+
+				// make bubbles to hole hero block
+				bubblesGenerator('.js-hero-bubbles', 200, 110)
+			}
 		})
 
 		setTimeout(() => {
 			bubble.remove()
 		}, duration + 1000)
-	}, 1000)
+	}, delay)
 }
 
-bubblesGenerator()
+bubblesGenerator('.js-bubbles', 1000, 40)
 // bubbles.js [END]
