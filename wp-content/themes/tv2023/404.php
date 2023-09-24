6af6b8f26c36a248aafd51230ddb0b15e92b6629
dfox <dfox@foxartbox.com>
2023-09-24 12:40:22 +0300

    404 page

---
+++ wp-content/themes/tv2023/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main class="main">
+	<div class="error_404">
+		<h1>404 - Page Not Found</h1>
+
+		<p><a class="btn btn__buy" href="/">Go to main page</a></p>
+	</div>
+</main>
+
+<?php get_footer(); ?>
