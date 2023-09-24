8756944247858c91224e24ee5b077f9043936099
dfox <dfox@foxartbox.com>
2023-09-24 19:04:13 +0300

    PHP. Product page. Base. Part 2

--- wp-content/themes/tv2023/single-product.php
+++ wp-content/themes/tv2023/single-product.php
@@ -1,12 +1,42 @@
 <?php get_header(); ?>
 
-	<main class="main">
-		<?php if (single_post_title()) { ?>
-			<h1><?php single_post_title(); ?></h1>
+<?php // attributes
+global $product;
+
+$gallery_images     = $product->get_gallery_image_ids();
+$attributes         = $product->get_attributes(); ?>
+
+<?php // object attributes
+$productId          = $product->get_id();
+$image              = wp_get_attachment_image_src( get_post_thumbnail_id( $productId ), 'single-post-thumbnail' );
+$price              = $product->get_price(); ?>
+
+<div class="product js-product" data-product-id="<?= $productId; ?>">
+	<div class="product_gallery owl-carousel js-owl-carousel">
+		<img class="product_gallery__img" src="<?= str_replace('https://' . $_SERVER['SERVER_NAME'],'', $image[0]); ?>" alt="">
+
+		<?php // gallery images
+		foreach ($gallery_images as $key => $gallery_image) {
+			// thumbnail | medium | large | full
+			$image_link = str_replace('https://' . $_SERVER['SERVER_NAME'], '', wp_get_attachment_image_url($gallery_image, 'large')); ?>
+
+			<img class="product_gallery__img js-product-gallery-img" src="<?= $image_link; ?>" alt="">
+		<?php } ?>
+	</div>
+
+	<div class="product_info">
+		<h3><?= $product->get_title(); ?></h3>
+
+		<?php // price
+		if ($price) { ?>
+			<div class="product_info__price"><?= $price; ?> €</div>
 		<?php } ?>
 
-		<?php // content
-		the_content(); ?>
-	</main>
+		<div class="product_info__desc">
+			<?php // content
+			the_content(); ?>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
