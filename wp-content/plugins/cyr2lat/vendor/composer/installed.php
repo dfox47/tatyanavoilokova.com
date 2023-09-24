51522c7c86b9adf78ec5d83ffc92c2d093e8bea5
dfox <dfox@foxartbox.com>
2023-09-24 13:04:37 +0300

    Plugins added to git

---
+++ wp-content/plugins/cyr2lat/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mihdan/cyr2lat',
+        'pretty_version' => '6.0.4',
+        'version' => '6.0.4.0',
+        'reference' => '2239cd9483564099542722d08135b1cf57de0a7b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mihdan/cyr2lat' => array(
+            'pretty_version' => '6.0.4',
+            'version' => '6.0.4.0',
+            'reference' => '2239cd9483564099542722d08135b1cf57de0a7b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
