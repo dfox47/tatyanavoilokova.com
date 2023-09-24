217103ae34572fe2d82d58f5450253b95e3da5d0
dfox <dfox@foxartbox.com>
2023-09-24 19:02:59 +0300

    JS. Hero. Updates

--- wp-content/themes/tv2023/js/hero.js
+++ wp-content/themes/tv2023/js/hero.js
@@ -1,12 +1,18 @@
 // hero.js [START]
-const $hero = document.querySelector('.js-hero')
+const heroBg = () => {
+	const $hero = document.querySelector('.js-hero')
 
-$hero.addEventListener('mousemove', e => {
-	Object.assign($hero, {
-		style: `
+	if (!$hero) return
+
+	$hero.addEventListener('mousemove', e => {
+		Object.assign($hero, {
+			style: `
 		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
 		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
 		`
+		})
 	})
-})
+}
+
+heroBg()
 // hero.js [END]
