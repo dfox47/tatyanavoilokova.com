718de150a24e2b8a6714218134cc3a63cb0f3180
dfox <dfox@foxartbox.com>
2023-09-24 19:03:42 +0300

    PHP. Popup template

--- wp-content/themes/tv2023/template-parts/popups.php
+++ wp-content/themes/tv2023/template-parts/popups.php
@@ -1,25 +1,25 @@
-<!-- feedback popup -->
-<div class="popup popup--feedback js-popup" data-popup="feedback">
-	<div class="popup__bg js-popup-close"></div>
+<?php // feedback popup
+if (shortcode_exists('[contact-form-7 id="63"]')) { ?>
+	<div class="popup popup--feedback js-popup" data-popup="feedback">
+		<div class="popup__bg js-popup-close"></div>
 
-	<div class="popup__content">
-		<div class="popup__close js-popup-close"></div>
+		<div class="popup__content">
+			<div class="popup__close js-popup-close"></div>
 
-		<div class="popup__content_wrap">
-			<div class="contacts_form">
-				<?php if ( shortcode_exists( '[contact-form-7 id="63"]' ) ) {
-					echo do_shortcode('[contact-form-7 id="63"]');
-				} ?>
+			<div class="popup__content_wrap">
+				<div class="contacts_form">
+					<?= do_shortcode('[contact-form-7 id="63"]'); ?>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+<?php } ?>
 
 <div class="popup popup--content js-popup" data-popup="with_content">
 	<div class="popup__bg js-popup-close"></div>
 
 	<div class="popup__content">
-		<div class="popup__close js-popup-close"></div>
+		<div class="btn btn__close js-popup-close"></div>
 
 		<div class="popup__content_wrap js-popup-content"></div>
 	</div>
