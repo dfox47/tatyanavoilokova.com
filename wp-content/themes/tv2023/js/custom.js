0bd6d30d6da3da546c2382d98c614d01424bc42e
dfox <dfox@foxartbox.com>
2023-09-26 15:03:34 +0300

    JS. Owl default autoplay true

--- wp-content/themes/tv2023/js/custom.js
+++ wp-content/themes/tv2023/js/custom.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 	// default owl carousel
 	$('.js-owl-carousel').owlCarousel({
 		autoplay:           true,
+		autoplayHoverPause: true,
 		autoplayTimeout:    3500,
 		items:              1,
 		loop:               true,
