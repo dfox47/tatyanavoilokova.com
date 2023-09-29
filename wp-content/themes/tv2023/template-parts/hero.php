c1d54dc733321bf5c2876c90bff9289466d78904
dfox <dfox@foxartbox.com>
2023-09-29 11:14:19 +0300

    Hero lang updates

--- wp-content/themes/tv2023/template-parts/hero.php
+++ wp-content/themes/tv2023/template-parts/hero.php
@@ -2,9 +2,26 @@
 	<div class="hero__bg"></div>
 
 	<div class="hero__content">
-		<div class="hero__title">Tatyana<br>Voilokova</div>
-		<div class="hero__subtitle">photographer</div>
-		<a class="btn btn__more js-scroll-to" href="#about">About me</a>
+		<div class="hero__title">
+			<span class="lang_bg_only">Татяна<br>Войлокова</span>
+			<span class="lang_en_only">Tatyana<br>Voilokova</span>
+			<span class="lang_gr_only">Τατιάνα<br>Βοϊλόκοβα</span>
+			<span class="lang_ru_only">Татьяна<br>Войлокова</span>
+		</div>
+
+		<div class="hero__subtitle">
+			<span class="lang_bg_only">фотограф</span>
+			<span class="lang_en_only">photographer</span>
+			<span class="lang_gr_only">φωτογράφος</span>
+			<span class="lang_ru_only">фотограф</span>
+		</div>
+
+		<a class="btn btn__more js-scroll-to" href="#about">
+			<span class="lang_bg_only">За мен</span>
+			<span class="lang_en_only">About me</span>
+			<span class="lang_gr_only">Σχετικά με μένα</span>
+			<span class="lang_ru_only">Обо мне</span>
+		</a>
 
 		<div class="bubbles js-bubbles"></div>
 	</div>
