e7ec2f5b161b2a3f31f4255f0f2c1136c60edadb
dfox <dfox@foxartbox.com>
2023-09-29 11:26:03 +0300

    Single product add https

--- wp-content/themes/tv2023/single-product.php
+++ wp-content/themes/tv2023/single-product.php
@@ -4,7 +4,9 @@
 global $product;
 
 $gallery_images     = $product->get_gallery_image_ids();
-$attributes         = $product->get_attributes(); ?>
+$attributes         = $product->get_attributes();
+$protocol           = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://'; ?>
+
 
 <?php // object attributes
 $productId          = $product->get_id();
@@ -13,12 +15,12 @@ $price              = $product->get_price(); ?>
 
 <div class="product js-product" data-product-id="<?= $productId; ?>">
 	<div class="product_gallery owl-carousel js-owl-carousel">
-		<img class="product_gallery__img" src="<?= str_replace('https://' . $_SERVER['SERVER_NAME'],'', $image[0]); ?>" alt="">
+		<img class="product_gallery__img" src="<?= str_replace($protocol . $_SERVER['SERVER_NAME'],'', $image[0]); ?>" alt="">
 
 		<?php // gallery images
 		foreach ($gallery_images as $key => $gallery_image) {
 			// thumbnail | medium | large | full
-			$image_link = str_replace('https://' . $_SERVER['SERVER_NAME'], '', wp_get_attachment_image_url($gallery_image, 'large')); ?>
+			$image_link = str_replace($protocol . $_SERVER['SERVER_NAME'], '', wp_get_attachment_image_url($gallery_image, 'large')); ?>
 
 			<img class="product_gallery__img js-product-gallery-img" src="<?= $image_link; ?>" alt="">
 		<?php } ?>
@@ -35,6 +37,25 @@ $price              = $product->get_price(); ?>
 		<div class="product_info__desc">
 			<?php // content
 			the_content(); ?>
+		</div>
+
+		<div class="product_form">
+			<label class="product_form__label">
+				<span class="product_form__title">Name</span>
+				<input class="product_form__input" type="text" name="user-name">
+			</label>
+
+			<label class="product_form__label">
+				<span class="product_form__title">Email</span>
+				<input class="product_form__input" type="text" name="user-email">
+			</label>
+
+			<label class="product_form__submit">
+				<span class="product_form__title">submit</span>
+				<input class="product_form__input" type="submit">
+			</label>
+
+			<input type="hidden" data-link="">
 		</div>
 	</div>
 </div>
