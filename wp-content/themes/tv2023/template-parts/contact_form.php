baf3f9a2d88683b286bb7fa102a0174d4758f718
dfox <dfox@foxartbox.com>
2023-09-30 21:57:30 +0300

    Contacts form PHP + CSS

---
+++ wp-content/themes/tv2023/template-parts/contact_form.php
@@ -0,0 +1,26 @@
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$to = 'dfox@foxartbox.com';
+
+	$subject    = '[tatyanavoilokova.com] Product order';
+	$userName   = $_POST['user-name'];
+	$userEmail  = $_POST['user-email'];
+	$productUrl = $_POST['product-link'];
+
+	$message = '<html lang="en"><body>';
+	$message .= '<p><strong>Имя: </strong>' . $userName . '</p>';
+	$message .= '<p><strong>Email: </strong>' . $userEmail . '</p>';
+	$message .= '<p><strong>Ссылка на продукт: </strong>' . $productUrl . '</p>';
+	$message .= '</body></html>';
+
+	$headers = 'From: info@tatyanavoilokova.com' . "\r\n" .
+		'Reply-To: sender@example.com' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion() . "\r\n" .
+		'Content-Type: text/html; charset=UTF-8';
+
+	if (mail($to, $subject, $message, $headers)) {
+		echo 'Email sent successfully.';
+	}
+	else {
+		echo 'Email could not be sent.';
+	}
+} ?>
