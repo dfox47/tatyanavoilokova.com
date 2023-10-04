15dfb308900d2a4f2fa54024c1753d26f4fb7519
dfox <dfox@foxartbox.com>
2023-10-04 19:24:03 +0300

    Menu translation

--- wp-content/themes/tv2023/template-parts/header_menu.php
+++ wp-content/themes/tv2023/template-parts/header_menu.php
@@ -1,9 +1,47 @@
 <div class="header_menu_toggle js-header-menu-toggle"></div>
 
 <ul class="header_menu">
-	<li><a class="js-scroll-to js-header-menu-toggle" href="#top">Home</a></li>
-	<li><a class="js-scroll-to js-header-menu-toggle" href="#about">About me</a></li>
-	<li><a class="js-scroll-to js-header-menu-toggle" href="#portfolio">Portfolio</a></li>
-	<li><a class="js-scroll-to js-header-menu-toggle" href="#contacts">Contacts</a></li>
-	<li><a class="js-scroll-to js-header-menu-toggle" href="#awards">Awards</a></li>
+	<li>
+		<a class="js-scroll-to js-header-menu-toggle" href="#top">
+			<span class="lang_bg_only">Главна</span>
+			<span class="lang_en_only">Home</span>
+			<span class="lang_gr_only">Σπίτι</span>
+			<span class="lang_ru_only">Главная</span>
+		</a>
+	</li>
+
+	<li>
+		<a class="js-scroll-to js-header-menu-toggle" href="#about">
+			<span class="lang_bg_only">За мен</span>
+			<span class="lang_en_only">About me</span>
+			<span class="lang_gr_only">Σχετικά με μένα</span>
+			<span class="lang_ru_only">Обо мне</span>
+		</a>
+	</li>
+
+	<li>
+		<a class="js-scroll-to js-header-menu-toggle" href="#portfolio">
+			<span class="lang_bg_only">Портфолио</span>
+			<span class="lang_en_only">Portfolio</span>
+			<span class="lang_gr_only">Χαρτοφυλάκιο</span>
+			<span class="lang_ru_only">Портфолио</span>
+		</a>
+	</li>
+
+	<li>
+		<a class="js-scroll-to js-header-menu-toggle" href="#contacts">
+			<span class="lang_bg_only">Контакти</span>
+			<span class="lang_en_only">Contacts</span>
+			<span class="lang_gr_only">Επαφές</span>
+			<span class="lang_ru_only">Контакты</span>
+		</a>
+	</li>
+
+	<li><a class="js-scroll-to js-header-menu-toggle" href="#awards">
+			<span class="lang_bg_only">Награди</span>
+			<span class="lang_en_only">Awards</span>
+			<span class="lang_gr_only">Βραβεία</span>
+			<span class="lang_ru_only">Награды</span>
+		</a>
+	</li>
 </ul>
