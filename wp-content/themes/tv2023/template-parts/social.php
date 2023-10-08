41b043ffead73da0777c1ccf17521e9115c937c5
dfox <dfox@foxartbox.com>
2023-10-08 15:07:14 +0300

    Social. Link to facebook update

--- wp-content/themes/tv2023/template-parts/social.php
+++ wp-content/themes/tv2023/template-parts/social.php
@@ -1,6 +1,6 @@
 <ul class="social_list">
 	<?php // all options https://tatyanavoilokova.com/wp-admin/admin.php?page=theme-custom-options
-	$facebook       = esc_attr(get_option('theme_facebook'))    ? esc_attr(get_option('theme_facebook'))    : '//www.facebook.com/profile.php?id=100001149043902';
+	$facebook       = esc_attr(get_option('theme_facebook'))    ? esc_attr(get_option('theme_facebook'))    : 'profile.php?id=100001149043902';
 	$instagram      = esc_attr(get_option('theme_inst'))        ? esc_attr(get_option('theme_inst'))        : 'tatiana.voil';
 	$telegram       = esc_attr(get_option('theme_telegram'))    ? esc_attr(get_option('theme_telegram'))    : 'Tatiana_Voilokova';
 	$whatsapp       = esc_attr(get_option('theme_whatsapp'))    ? esc_attr(get_option('theme_whatsapp'))    : '359876700150'; ?>
