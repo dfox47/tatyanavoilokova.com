d4d7360f58d7912aaafeb91ff2ed14faa4e01cd2
dfox <dfox@foxartbox.com>
2023-10-08 16:43:13 +0300

    Products home shortcut updates

--- wp-content/themes/tv2023/template-parts/products_home.php
+++ wp-content/themes/tv2023/template-parts/products_home.php
@@ -1,67 +1,54 @@
 <?php
 // Custom shortcode function
-function products_home_function() { ?>
-	<div id="products" class="products_home js-hash-on-scroll">
-		<?php // put categories from different languages
-		$category_ids = array(17, 20);
+function products_home_function() {
+	$output = '';
 
-		$args = array(
-			'post_type'         => 'product',
-			'posts_per_page'    => -1,
-			'tax_query'         => array(
-				array(
-					'taxonomy'  => 'product_cat',
-					'field'     => 'id',
-					'terms'     => $category_ids,
-					'operator'  => 'IN'
-				),
+	// put categories from different languages
+	$category_ids = array(17, 20);
+
+	$args = array(
+		'post_type'         => 'product',
+		'posts_per_page'    => -1,
+		'tax_query'         => array(
+			array(
+				'taxonomy'  => 'product_cat',
+				'field'     => 'id',
+				'terms'     => $category_ids,
+				'operator'  => 'IN'
 			),
-		);
+		),
+	);
 
-		$products_query = new WP_Query($args);
+	$products_query = new WP_Query($args);
 
-		if ($products_query->have_posts()) : ?>
-			<h2>
-				<span class="lang_bg_only">Продукти</span>
-				<span class="lang_en_only">Products</span>
-				<span class="lang_gr_only">Προϊόντα</span>
-				<span class="lang_ru_only">Продукты</span>
-			</h2>
+	if ($products_query->have_posts()) :
+		$output .= '<div class="products_home__list">';
 
-			<div class="products_home__list">
-				<?php while ($products_query->have_posts()) : $products_query->the_post(); ?>
-					<div class="product_home">
-						<?php global $product;
-						$id     = $product->get_id();
-						$url    = get_permalink(); ?>
+		while ($products_query->have_posts()) : $products_query->the_post();
+			global $product;
 
-						<h3><?php the_title(); ?></h3>
+			$id     = $product->get_id();
+			$url    = get_permalink();
+			$image  = wp_get_attachment_image_src(get_post_thumbnail_id($id), 'single-post-thumbnail');
 
-						<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($id), 'single-post-thumbnail'); ?>
+			$output .= '<div class="product_home">';
+			$output .= '<h3>' . $product->get_title() . '</h3>';
+			$output .= '<a class="product_home__img_wrap js-product-home-more" href="' . $url . '">';
+			$output .= '<span class="product_home__img js-img-scroll" data-src="' . str_replace('https://' . $_SERVER['HTTP_HOST'], '', $image[0]) . '"></span>';
+			$output .= '</a>';
+			$output .= '<p class="product_home__desc">' . $product->post->post_excerpt . '</p>';
+			$output .= '<div class="product_home__more">';
+			$output .= '<a class="btn btn__buy js-product-home-more" href="' . $url . '"><span class="lang_bg_only">Повече</span><span class="lang_en_only">More</span><span class="lang_gr_only">Περισσότερες</span><span class="lang_ru_only">Подробнее</span></a>';
+			$output .= '</div></div>';
+		endwhile;
 
-						<a class="product_home__img_wrap js-product-home-more" href="<?= $url; ?>">
-							<span class="product_home__img js-img-scroll" data-src="<?= str_replace('https://' . $_SERVER['HTTP_HOST'], '', $image[0]); ?>"></span>
-						</a>
+		$output .= '</div>';
 
-						<div class="product_home__desc"><?php the_excerpt(); ?></div>
+		wp_reset_postdata();
+	endif;
 
-						<div class="product_home__more">
-							<a class="btn btn__buy js-product-home-more" href="<?= $url; ?>">
-								<span class="lang_bg_only">Повече</span>
-								<span class="lang_en_only">More</span>
-								<span class="lang_gr_only">Περισσότερες</span>
-								<span class="lang_ru_only">Подробнее</span>
-
-							</a>
-						</div>
-					</div>
-				<?php endwhile;
-
-				wp_reset_postdata(); ?>
-			</div>
-		<?php endif; ?>
-	</div>
-<?php }
+	return $output;
+}
 
 // Register the shortcode
 add_shortcode('products_home', 'products_home_function');
