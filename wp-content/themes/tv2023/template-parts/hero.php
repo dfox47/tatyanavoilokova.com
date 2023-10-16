792c039e3994fd0db0797d53aa6dfd633bcf0059
dfox <dfox@foxartbox.com>
2023-10-16 21:27:41 +0300

    Hero. Title animation

--- wp-content/themes/tv2023/template-parts/hero.php
+++ wp-content/themes/tv2023/template-parts/hero.php
@@ -5,10 +5,25 @@
 
 	<div class="hero__content">
 		<div class="hero__title">
-			<span class="lang_bg_only">Татяна<br>Войлокова</span>
-			<span class="lang_en_only">Tatyana<br>Voilokova</span>
-			<span class="lang_gr_only">Τατιάνα<br>Βοϊλόκοβα</span>
-			<span class="lang_ru_only">Татьяна<br>Войлокова</span>
+			<span class="lang_bg_only">
+				<span class="hero__title_text">Татяна<br><span class="hero__title_text--full">Войлокова</span></span>
+				<span class="hero__title_bg">Татяна</span>
+			</span>
+
+			<span class="lang_en_only">
+				<span class="hero__title_text">Tatyana<br><span class="hero__title_text--full">Voilokova</span></span>
+				<span class="hero__title_bg">Tatyana</span>
+			</span>
+
+			<span class="lang_gr_only">
+				<span class="hero__title_text">Τατιάνα<br><span class="hero__title_text--full">Βοϊλόκοβα</span></span>
+				<span class="hero__title_bg">Τατιάνα</span>
+			</span>
+
+			<span class="lang_ru_only">
+				<span class="hero__title_text">Татьяна<br><span class="hero__title_text--full">Войлокова</span></span>
+				<span class="hero__title_bg">Татьяна</span>
+			</span>
 		</div>
 
 		<div class="hero__subtitle">
