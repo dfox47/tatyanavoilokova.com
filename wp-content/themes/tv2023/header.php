1cf1663a87fb3aecfde41af9f28b3e6daa077d51
dfox <dfox@foxartbox.com>
2023-10-18 22:40:34 +0300

    Fonts for page speed

--- wp-content/themes/tv2023/header.php
+++ wp-content/themes/tv2023/header.php
@@ -45,6 +45,10 @@ if ($sites) {
 
 		<?php wp_head(); ?>
 
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
 		<link rel="stylesheet" href="<?= esc_url(get_template_directory_uri()); ?>/style.css?v<?= (date("YmdHis")); ?>" />
 	</head>
 <body>
